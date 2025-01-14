9f5db47a53a908b7a44c55b7230fe7d1e8d7c00e
aprianfirlanda <aprianfirlanda@gmail.com>
2025-01-14 04:07:03 +0000

    fix(input): forget export component InputSearchDebounce

--- inputs/index.js
+++ inputs/index.js
@@ -1,5 +1,6 @@
 export { default as Checkbox } from './Checkbox.jsx';
 export { default as InputSearch } from './InputSearch.jsx';
+export { default as InputSearchDebounce } from './InputSearchDebounce.jsx';
 export { default as InputText } from './InputText.jsx';
 export { default as Radio } from './Radio.jsx';
 export { default as Select } from './Select.jsx';
