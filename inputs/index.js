b3e9aa815c7230db3e23be84b81491f691efee0e
aprianfirlanda <aprianfirlanda@gmail.com>
2025-01-14 03:44:44 +0000

    fix(Input/Textarea): wrong name

--- inputs/index.js
+++ inputs/index.js
@@ -3,4 +3,4 @@ export { default as InputSearch } from './InputSearch.jsx';
 export { default as InputText } from './InputText.jsx';
 export { default as Radio } from './Radio.jsx';
 export { default as Select } from './Select.jsx';
-export { default as TextArea } from './TextArea.jsx';
+export { default as Textarea } from './Textarea.jsx';
